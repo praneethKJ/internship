d0bba162d38d0db8c75294d1733eebbf6f04c4da
praneeth <kodadalajp0503@gmail.com>
2023-09-12 10:54:27 +0530

    third commit

---
+++ asserts/js/profile.js
@@ -0,0 +1,7 @@
+//js for typed script
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Engineer', '&amp; Web Designer'],
+    typeSpeed: 50,
+    loop: true,
+    backSpeed: 40,
+});
